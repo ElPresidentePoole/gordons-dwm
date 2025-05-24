ebd05b34437727cf9126504ca5d96227b8947fb6
Gordon Poole <gpoole@posteo.net>
2025-05-23 23:10:03 -0500

    take 2

--- dist/dwm-status.sh
+++ dist/dwm-status.sh
@@ -1,1 +1,19 @@
-/home/gpoole/Opt/dwm-status.sh
+#!/usr/bin/env sh
+
+while true; do
+    STATUS="$(cat /sys/class/power_supply/BAT0/status)"
+    if [ "$STATUS" = "Discharging" ]; then
+        # no icon
+        STATUS="󰁹"
+    elif [ "$STATUS" = "Charging" ]; then
+        # lightning bolt
+        STATUS="󰂄"
+    elif [ "$STATUS" = "Not Charging" ]; then
+        # full battery icon
+        STATUS="󰂄"
+    fi
+
+    xsetroot -name "$(date) $STATUS$(cat /sys/class/power_supply/BAT0/capacity)"
+    sleep 1
+done
+
